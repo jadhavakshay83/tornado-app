6629ae4bc48dcfbbb4b75ad6d4396cdd2077e7a3
SumedhaZestl <sumedha@zestl.com>
2019-08-13 17:50:01 +0530

    function handler with positional parameters in url #1 Testing of positional parameters in urls and returning an array from response

--- botHandler.py
+++ botHandler.py
@@ -2,10 +2,18 @@ from tornado.web import RequestHandler
 from httpClient import HttpClientHandler
 import json
 import constants
+from FunctionHandler import FunctionHandler
+
 
 class BotHandler(RequestHandler):
   async def get(self):
-    httpClient = HttpClientHandler(businessTagId = "42FNCQGRPLJJ6", relativeUrl = "/user/tasks/get/"+constants.BUSINESS_TAG_ID, method = "GET")
-    resp = await httpClient.processRequest()
+    # httpClient = HttpClientHandler(businessTagId = "42FNCQGRPLJJ6", relativeUrl = "/user/tasks/get/"+constants.BUSINESS_TAG_ID, method = "GET")
+    # resp = await httpClient.processRequest()
+    payload=[
+        {
+            "FieldLabel": "OYO ID",
+            "Value": "FAR040"}]
+    resp=await FunctionHandler.get_master_entry(formID="5",payload=payload)
     self.write(resp)
     self.write({'message': 'Bot handler called', 'status':'Active'})
+
